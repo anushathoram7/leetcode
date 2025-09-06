b4fcb89b34347431a5adab241587aac7cf83f934
anushathoram7 <anushathoram3@gmail.com>
2025-09-06 14:45:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ 2608-count-the-digits-that-divide-a-number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        a=str(num)
+        count=0
+        for i in a:
+            if num% int(i)==0:
+               count+=1
+        return count
+        

aebb52f59c73e2aa74fede4e387ddc17c0c12d08
anushathoram7 <anushathoram3@gmail.com>
2025-09-06 15:01:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (100.00%)

---
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        count=0
+        sum1=0
+        for num in nums:
+          if num%6==0:
+            sum1+=num
+            count+=1
+        if count==0:
+            return 0
+        else: 
+            return sum1//count
+            
+
+
+        

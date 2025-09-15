13ed1f5f1fdcd216b87d7fffadb2da1f345bc841
anushathoram7 <anushathoram3@gmail.com>
2025-09-15 04:39:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ 1560-number-of-students-doing-homework-at-a-given-time/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def busyStudent(self, startTime: list[int], endTime: list[int], queryTime: int) -> int:
+        count = 0
+        for s, e in zip(startTime, endTime):
+            if s <= queryTime <= e:
+                count += 1
+        return count

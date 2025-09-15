99526e32154aaaade5d4c6332080a9a0364dc2dc
anushathoram7 <anushathoram3@gmail.com>
2025-09-15 04:24:57 +0000

    Sync LeetCode submission Runtime - 4 ms (23.60%), Memory - 18 MB (100.00%)

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        d = {}
+
+        for ch in s:
+            if ch in d:
+                d[ch] += 1
+            else:
+                d[ch] = 1
+
+        # If all values in d are the same, set of values will have length 1
+        return len(set(d.values())) == 1

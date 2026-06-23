9e23767082c8d0fb04475b80a92c7e6dc717caed
anushathoram7 <anushathoram3@gmail.com>
2026-06-23 04:56:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20 MB (54.73%)

---
+++ 0860-design-circular-queue/solution.py
@@ -0,0 +1,35 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q = []
+        self.capacity = k
+
+    def enQueue(self, value: int) -> bool:
+        if len(self.q) == self.capacity:
+            return False
+
+        self.q.append(value)
+        return True
+
+    def deQueue(self) -> bool:
+        if not self.q:
+            return False
+
+        self.q.pop(0)
+        return True
+
+    def Front(self) -> int:
+        if not self.q:
+            return -1
+        return self.q[0]
+
+    def Rear(self) -> int:
+        if not self.q:
+            return -1
+        return self.q[-1]
+
+    def isEmpty(self) -> bool:
+        return len(self.q) == 0
+
+    def isFull(self) -> bool:
+        return len(self.q) == self.capacity
